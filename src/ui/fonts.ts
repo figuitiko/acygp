7e9a581cfe32ca39d38b439cae6609e1917e85bd
frank Freeman <frankfigao@gmail.com>
2025-05-24 09:21:14 -0600

    chore: first commit

---
+++ src/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Fira_Sans } from "next/font/google";
+
+export const firaSans = Fira_Sans({
+  subsets: ["latin"],
+  variable: "--font-fira-sans",
+  weight: ["400", "500", "600", "700"],
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        main: "var(--main-color)",
+        lavender: "var(--lavender)",
       },
     },
   },
